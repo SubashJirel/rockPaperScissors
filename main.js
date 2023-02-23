f84f12812366546e3eae220ae269caacd62b89c4
Subash Jirel <subashjrl@gmail.com>
2023-02-23 15:20:33 +0545

    added game function to play the game best of 5 using prompt in browser

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    while(playerScore<5 && computerScore<5) {
+        let playerSelection = prompt("Enter your choice").toLowerCase()
+        let computerSelection= getComputerChoice().toLowerCase()
+        let win = checkWin(playerSelection,computerSelection)
+
+        if(win == 'player') playerScore++
+        else if(win == 'computer') computerScore++
+        console.log(`Player Score: ${playerScore} \n Computer Score: ${computerScore}`)     
+    }
+    
+    if(playerScore ==5) {
+        console.log("player won")
+    }
+    else if(computerScore ==5)
+        console.log("Computer won")
+    else console.error("player score or computer score is not 5")
+    
+  }
+
 function checkWin(playerSelection, computerSelection) { //checkWIn
     // your code here!
     if((playerSelection == 'rock'&& computerSelection == 'scissors') || (playerSelection == 'scissors'&& computerSelection == 'paper') || (playerSelection == 'paper'&& computerSelection == 'rock'))
@@ -12,7 +35,6 @@ function checkWin(playerSelection, computerSelection) { //checkWIn
     }
   }
 
- 
 function getComputerChoice() {
     let rand = Math.random();
     if(rand <0.333333) {
@@ -25,6 +47,4 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(checkWin(playerSelection, computerSelection));
+game()
