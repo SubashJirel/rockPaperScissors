b571ee6e04c3c8d0af8515604d9dce37927ddbb9
Subash Jirel <subashjrl@gmail.com>
2023-02-26 20:13:15 +0545

    reomved prompt, made result to be displayed in the webpage instead of console

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
 
 function game(event) {
-    let playerScore = 0
-    let computerScore = 0
-    while(playerScore<5 && computerScore<5) {
-        let playerSelection = event.target.innerText;
-        let computerSelection= getComputerChoice().toLowerCase()
-        let win = checkWin(playerSelection,computerSelection)
+    //getting player and computer score from the span
+    let playerScore = Number(document.querySelector('.playerScoreDisplay').innerText)
+    let computerScore = Number(document.querySelector('.computerScoreDisplay').innerText)
 
-        if(win == 'player') playerScore++
-        else if(win == 'computer') computerScore++
-        console.log(`Player Score: ${playerScore} \n Computer Score: ${computerScore}`)     
+    let playerSelection = event.target.innerText; //player slection from the button being clicked
+    let computerSelection= getComputerChoice().toLowerCase()
+    let win = checkWin(playerSelection.toLowerCase(),computerSelection.toLowerCase())
+
+    if(win == 'player') {
+        playerScore++
+        document.querySelector('.playerScoreDisplay').innerText = playerScore
     }
+    else if(win == 'computer') {
+        computerScore++
+        document.querySelector('.computerScoreDisplay').innerText = computerScore
+    }
+   
+    console.log(`Player Score: ${playerScore} \n Computer Score: ${computerScore}`)     
+
     
     if(playerScore ==5) {
+        document.querySelector('.result').innerText ="Final Result: player won"
         console.log("player won")
+        allButtons.forEach(btn => btn.classList.toggle('hide'))
+
     }
-    else if(computerScore ==5)
+    else if(computerScore ==5) {
+        document.querySelector('.result').innerText ="Final Result: Computer won"
         console.log("Computer won")
-    else console.error("player score or computer score is not 5")
+        allButtons.forEach(btn => btn.classList.toggle('hide'))
+
+    }
+  
     
   }
 
 function checkWin(playerSelection, computerSelection) { //checkWIn
     // your code here!
     if((playerSelection == 'rock'&& computerSelection == 'scissors') || (playerSelection == 'scissors'&& computerSelection == 'paper') || (playerSelection == 'paper'&& computerSelection == 'rock'))
-    {
+    {   
+        document.querySelector('.commentary').innerText = `You won this round!! ${playerSelection} beats ${computerSelection}`
         console.log(`You won this round!! ${playerSelection} beats ${computerSelection}`)
         return 'player'
     }
     else if(playerSelection == computerSelection) {
+        document.querySelector('.commentary').innerText = `It's a draawww!!!`
         console.log(`It's a draawww!!!`)
         return 'draw'
     }
     else {
+        document.querySelector('.commentary').innerText = `You lost this round!! ${computerSelection} beats ${playerSelection}`
         console.log(`You lost this round!! ${computerSelection} beats ${playerSelection}`)
         return 'computer'
     }
@@ -50,7 +68,7 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
-// game()
+
 const rock = document.querySelector('.rockButton')
 const paper = document.querySelector('.paperButton')
 const scissor = document.querySelector('.scissorButton')
@@ -70,6 +88,9 @@ function startGame(event) {
     document.querySelector('.startGame').classList.toggle('hide')
     allButtons.forEach(btn => btn.classList.toggle('hide'))
 
+    document.querySelector('.scores').classList.toggle('hide')
+
+
 }
 
 //reset the game 
@@ -78,7 +99,19 @@ document.querySelector('.resetGame').addEventListener('click',resetGame)
 function resetGame(event) {
     document.querySelector('.startGame').classList.toggle('hide')
     document.querySelector('.resetGame').classList.toggle('hide')
-    allButtons.forEach(btn => btn.classList.toggle('hide'))
+
+    allButtons.forEach(btn => btn.classList.add('hide'))
+
+    document.querySelector('.scores').classList.toggle('hide')
+
+    //making scores 0
+    document.querySelector('.playerScoreDisplay').innerText = 0
+    document.querySelector('.computerScoreDisplay').innerText = 0
+
+    //reseting commentary and results
+    document.querySelector('.commentary').innerText = ""
+    document.querySelector('.result').innerText = ""
+    
 
 
 }
